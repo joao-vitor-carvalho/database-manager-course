cf1956105bc8daaf0f81c9bfe2a47cea88a593aa
joao-vitor-carvalho <91700668+joao-vitor-carvalho@users.noreply.github.com>
2021-10-04 20:54:03 -0300

    Create tbl_empresa.sql

---
+++ tbl_empresa.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Empresa(
+codempr varchar(6) not null,
+nome varchar(20),
+endereco varchar(45),
+PRIMARY KEY(codempr)
+)
+ENGINE = MyISAM default charset = utf8;
+
+SHOW TABLES;
+
+SELECT *
+FROM Empresa;
+
+SELECT *
+FROM Aluno;
+
+SELECT *
+FROM Disciplina;
